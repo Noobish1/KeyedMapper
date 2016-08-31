2a56a808d141d599ace7d12c68f3035716d605e4
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-08-31 08:36:53 -0500

    gave ConvertibleError a failureReason.

--- KeyedMapper/Objects/ConvertibleError.swift
+++ KeyedMapper/Objects/ConvertibleError.swift
@@ -3,4 +3,8 @@ import Foundation
 public struct ConvertibleError: Error {
     public let value: AnyObject?
     public let type: Any.Type
+    
+    public var failureReason: String {
+        return "Could not convert \(value) to type \(type)"
+    }
 }
