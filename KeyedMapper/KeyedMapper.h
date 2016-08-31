8613f79942253b81fa5ac0431d1a4cd2c28a8a52
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-08-30 23:15:16 -0500

    Initial Commit

---
+++ KeyedMapper/KeyedMapper.h
@@ -0,0 +1,19 @@
+//
+//  KeyedMapper.h
+//  KeyedMapper
+//
+//  Created by Blair McArthur on 30/08/16.
+//  Copyright © 2016 Noobish1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyedMapper.
+FOUNDATION_EXPORT double KeyedMapperVersionNumber;
+
+//! Project version string for KeyedMapper.
+FOUNDATION_EXPORT const unsigned char KeyedMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyedMapper/PublicHeader.h>
+
+
