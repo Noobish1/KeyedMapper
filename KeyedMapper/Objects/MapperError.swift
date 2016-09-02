c44cfc7c0707cb075377ef2c6558b89fdf247e77
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-01 23:31:27 -0500

    reordered the cases in the switch statement for the failureReason computed property in the MapperError enum and added a case for the customError which let me delete the default and TODO in it.

--- KeyedMapper/Objects/MapperError.swift
+++ KeyedMapper/Objects/MapperError.swift
@@ -9,17 +9,16 @@ public enum MapperError<Object: Mappable>: Error {
     
     public var failureReason: String {
         switch self {
+            case .customError(_, message: let message):
+                return message
+            case .invalidRawValueError(field: let field, forType: let type, value: let value, expectedType: let expectedType):
+                return "Invalid raw value for field \(field) of type \(type), \"\(value)\" is not a valid rawValue of \(expectedType)"
             case .missingFieldError(field: let field, forType: let type):
                 return "Missing field \(field.stringValue) of type \(type)"
             case .typeMismatchError(field: let field, forType: let type, value: let value, expectedType: let expectedType):
                 return "Type mismatch for field \(field) of type \(type), \"\(value)\" is a \(type(of: value)) but is expected to be \(expectedType)"
-            case .invalidRawValueError(field: let field, forType: let type, value: let value, expectedType: let expectedType):
-                return "Invalid raw value for field \(field) of type \(type), \"\(value)\" is not a valid rawValue of \(expectedType)"
             case .rootTypeMismatchError(forType: let type, value: let value, expectedType: let expectedType):
                 return "Type mismatch, root object \(value) of type \(type), expected to be of type \(expectedType)"
-            default:
-                //TODO: implement the rest
-                return "failureReason not implemented"
         }
     }
 }
