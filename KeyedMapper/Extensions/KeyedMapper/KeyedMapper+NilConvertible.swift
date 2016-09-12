2377d80a039169dc208b6637da2eb2e3270ba38e
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-11 21:31:44 -0500

    added NilConvertible, a protocol which is adopted by objects that want to convert nil into some other meaning

---
+++ KeyedMapper/Extensions/KeyedMapper/KeyedMapper+NilConvertible.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public extension KeyedMapper {
+    public func from<T: NilConvertible>(_ field: Object.Key) throws -> T where T == T.ConvertedType {
+        return try T.fromMap(try? self.JSONFromField(field))
+    }
+}
