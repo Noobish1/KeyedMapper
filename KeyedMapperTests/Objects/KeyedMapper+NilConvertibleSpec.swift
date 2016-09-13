9ab02e277986d253f56a0a999fe6b262bfae5cc5
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-12 22:37:16 -0500

    Added some tests for NilConvertible.

---
+++ KeyedMapperTests/Objects/KeyedMapper+NilConvertibleSpec.swift
@@ -0,0 +1,68 @@
+import Foundation
+import Quick
+import Nimble
+@testable import KeyedMapper
+
+fileprivate enum NilConvertibleEnum: NilConvertible {
+    case nothing
+    case something
+    
+    fileprivate static func fromMap(_ value: AnyObject?) throws -> NilConvertibleEnum {
+        return value.map { _ in .something } ?? .nothing
+    }
+}
+
+extension NilConvertibleEnum: Equatable {}
+
+fileprivate func == (lhs: NilConvertibleEnum, rhs: NilConvertibleEnum) -> Bool {
+    switch lhs {
+        case .nothing:
+            switch rhs {
+                case .nothing: return true
+                case .something: return false
+            }
+        case .something:
+            switch rhs {
+                case .nothing: return false
+                case .something: return true
+            }
+    }
+}
+
+fileprivate struct Model: Mappable {
+    fileprivate enum Key: String, JSONKey {
+        case enumProperty
+    }
+    
+    fileprivate let enumProperty: NilConvertibleEnum
+    
+    fileprivate init(map: KeyedMapper<Model>) throws {
+        try self.enumProperty = map.from(.enumProperty)
+    }
+}
+
+class KeyedMapperNilConvertibleSpec: QuickSpec {
+    override func spec() {
+        describe("KeyedMapper+NilConvertible") {
+            describe("from<T: NilConvertible>") {
+                context("when the given JSON does not contain the value") {
+                    it("should map correctly to the nothing case") {
+                        let JSON: NSDictionary = [:]
+                        let model = try! Model.from(JSON: JSON)
+                        
+                        expect(model.enumProperty) == NilConvertibleEnum.nothing
+                    }
+                }
+                
+                context("when the given JSON does contain the value") {
+                    it("should map correctly to the something case") {
+                        let JSON: NSDictionary = ["enumProperty": ""]
+                        let model = try! Model.from(JSON: JSON)
+                        
+                        expect(model.enumProperty) == NilConvertibleEnum.something
+                    }
+                }
+            }
+        }
+    }
+}
