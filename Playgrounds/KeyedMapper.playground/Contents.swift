898d3b2389410fb0b00b6d77e144216a28f561c3
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-17 17:19:26 -0500

    Instead of multiple playgrounds we're going to go with one KeyedMapper playground that introduces all concepts.

---
+++ Playgrounds/KeyedMapper.playground/Contents.swift
@@ -0,0 +1,110 @@
+import UIKit
+import KeyedMapper
+
+// Convertible
+
+extension NSTimeZone: Convertible {
+    public static func fromMap(_ value: Any) throws -> NSTimeZone {
+        guard let name = value as? String else {
+            throw MapperError.convertibleError(value: value, expectedType: String.self)
+        }
+        
+        guard let timeZone = self.init(name: name) else {
+            throw MapperError.customError(field: nil, message: "Unsupported timezone \(name)")
+        }
+        
+        return timeZone
+    }
+}
+
+// NilConvertible
+
+enum NilConvertibleEnum {
+    case something
+    case nothing
+}
+
+extension NilConvertibleEnum: NilConvertible {
+    static func fromMap(_ value: Any?) throws -> NilConvertibleEnum {
+        if let _ = value {
+            return .something
+        } else {
+            return .nothing
+        }
+    }
+}
+
+// Mappable
+
+struct SubObject {
+    let property: String
+}
+
+extension SubObject: Mappable {
+    enum Key: String, JSONKey {
+        case property
+    }
+
+    init(map: KeyedMapper<SubObject>) throws {
+        self.property = try map.from(.property)
+    }
+}
+
+struct Object {
+    let property: String
+    let optionalProperty: String?
+    let convertibleProperty: NSTimeZone
+    let optionalConvertibleProperty: NSTimeZone?
+    let nilConvertibleProperty: NilConvertibleEnum
+    let arrayProperty: [String]
+    let optionalArrayProperty: [String]?
+}
+
+extension Object: Mappable {
+    enum Key: String, JSONKey {
+        case property
+        case optionalProperty
+        case convertibleProperty
+        case optionalConvertibleProperty
+        case nilConvertibleProperty
+        case arrayProperty
+        case optionalArrayProperty
+    }
+    
+    init(map: KeyedMapper<Object>) throws {
+        self.property = try map.from(.property)
+        self.optionalProperty = map.optionalFrom(.optionalProperty)
+        self.convertibleProperty = try map.from(.convertibleProperty)
+        self.optionalConvertibleProperty = map.optionalFrom(.optionalConvertibleProperty)
+        self.nilConvertibleProperty = try map.from(.nilConvertibleProperty)
+        self.arrayProperty = try map.from(.arrayProperty)
+        self.optionalArrayProperty = map.optionalFrom(.optionalArrayProperty)
+    }
+}
+
+let JSON: [AnyHashable : Any] = ["property" : "propertyValue",
+                                 "convertibleProperty" : NSTimeZone(forSecondsFromGMT: 0).abbreviation,
+                                 "arrayProperty": ["arrayPropertyValue1", "arrayPropertyValue2"]]
+
+let object = try Object.from(JSON: JSON)
+
+print(object)
+
+// ReverseMappable
+
+extension Object: ReverseMappable {
+    func toKeyedJSON() -> [Object.Key : Any?] {
+        return [.property : property,
+                .optionalProperty : optionalProperty,
+                .convertibleProperty : convertibleProperty,
+                .optionalConvertibleProperty : optionalConvertibleProperty,
+                .nilConvertibleProperty : nilConvertibleProperty,
+                .arrayProperty : arrayProperty,
+                .optionalArrayProperty : optionalArrayProperty
+        ]
+    }
+}
+
+let outJSON = object.toJSON()
+
+print(outJSON)
