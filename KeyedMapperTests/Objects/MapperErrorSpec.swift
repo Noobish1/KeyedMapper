fdd60523eacce611ddc4b8e3b20f79891a80a64e
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-16 22:52:53 -0500

    Added more MapperError tests.

--- KeyedMapperTests/Objects/MapperErrorSpec.swift
+++ KeyedMapperTests/Objects/MapperErrorSpec.swift
@@ -39,6 +39,42 @@ class MapperErrorSpec: QuickSpec {
                         expect(error.failureReason) == expectedMessage
                     }
                 }
+                
+                context("a missingFieldError") {
+                    it("should return the correct message") {
+                        let field: Model.Key = .stringProperty
+                        let type = Model.self
+                        let expectedMessage = "Missing field \(field.stringValue) of type \(type)"
+                        let error = MapperError<Model>.missingFieldError(field: field, forType: type)
+                        
+                        expect(error.failureReason) == expectedMessage
+                    }
+                }
+                
+                context("a typeMismatchError") {
+                    it("should return the correct message") {
+                        let field: Model.Key = .stringProperty
+                        let type = Model.self
+                        let value = ""
+                        let expectedType = NSDictionary.self
+                        let expectedMessage = "Type mismatch for field \(field) of type \(type), \"\(value)\" is a \(type(of: value)) but is expected to be \(expectedType)"
+                        let error = MapperError<Model>.typeMismatchError(field: field, forType: type, value: value, expectedType: expectedType)
+                        
+                        expect(error.failureReason) == expectedMessage
+                    }
+                }
+                
+                context("a rootTypeMismatchError") {
+                    it("should return the correct message") {
+                        let type = Model.self
+                        let value = ""
+                        let expectedType = NSDictionary.self
+                        let expectedMessage = "Type mismatch, root object \(value) of type \(type), expected to be of type \(expectedType)"
+                        let error = MapperError<Model>.rootTypeMismatchError(forType: type, value: value, expectedType: expectedType)
+                        
+                        expect(error.failureReason) == expectedMessage
+                    }
+                }
             }
         }
     }
