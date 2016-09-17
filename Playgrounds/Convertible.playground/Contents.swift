bb2b32d7fcad09aa5855aa1cd38049668a82fbd1
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-17 16:08:22 -0500

    Rewrote the Convertible playground to use an NSTimeZone example instead of CLLocationCoordinate2D because the coordinate example could just be Mappable.

--- Playgrounds/Convertible.playground/Contents.swift
+++ Playgrounds/Convertible.playground/Contents.swift
@@ -2,42 +2,33 @@ import UIKit
 import CoreLocation
 import KeyedMapper
 
-extension CLLocationCoordinate2D: Convertible {
-    public static func fromMap(_ value: Any) throws -> CLLocationCoordinate2D {
-        guard let latlong = value as? NSDictionary else {
-            throw ConvertibleError(value: value, type: NSDictionary.self)
+// Implementation
+
+extension NSTimeZone: Convertible {
+    public static func fromMap(_ value: Any) throws -> NSTimeZone {
+        guard let name = value as? String else {
+            throw MapperError.convertibleError(value: value, expectedType: String.self)
         }
         
-        guard let lat = latlong["lat"] as? Double else {
-            throw ConvertibleError(value: latlong["lat"], type: Double.self)
+        guard let timeZone = self.init(name: name) else {
+            throw MapperError.customError(field: nil, message: "Unsupported timezone \(name)")
         }
         
-        guard let long = latlong["lng"] as? Double else {
-            throw ConvertibleError(value: latlong["lng"], type: Double.self)
-        }
-        
-        return CLLocationCoordinate2D(latitude: lat, longitude: long)
+        return timeZone
     }
 }
 
+// Example usage
 
-let badLatDict: Any = ["lat" : "NotADouble", "lng" : 2.3]
+let badValue = 2
 
 do {
-    let _ = try CLLocationCoordinate2D.fromMap(badLatDict)
-} catch let error as ConvertibleError {
+    let _ = try NSTimeZone.fromMap(badValue)
+} catch let error as MapperError {
     print(error.failureReason)
 }
 
-let badLngDict: Any = ["lat" : 2.3, "lng" : "NotADouble"]
+let goodValue = NSTimeZone(forSecondsFromGMT: 0).abbreviation!
+let timezone = try NSTimeZone.fromMap(goodValue)
 
-do {
-    let _ = try CLLocationCoordinate2D.fromMap(badLngDict)
-} catch let error as ConvertibleError {
-    print(error.failureReason)
-}
-
-let goodDict: Any = ["lat" : 2.3, "lng" : 4.4]
-let coordinate = try CLLocationCoordinate2D.fromMap(goodDict)
-
-print(coordinate)
+print(timezone)
