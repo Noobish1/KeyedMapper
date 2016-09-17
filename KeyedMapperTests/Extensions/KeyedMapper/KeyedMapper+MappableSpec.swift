bc68f9fe59ee71aaaa7377fd344cab97a149121c
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-09-16 22:20:32 -0500

    Added some tests for from<T: Mappable> -> [T].

--- KeyedMapperTests/Extensions/KeyedMapper/KeyedMapper+MappableSpec.swift
+++ KeyedMapperTests/Extensions/KeyedMapper/KeyedMapper+MappableSpec.swift
@@ -17,12 +17,15 @@ fileprivate struct SubModel: Mappable {
 fileprivate struct Model: Mappable {
     fileprivate enum Key: String, JSONKey {
         case mappableProperty
+        case arrayMappableProperty
     }
     
     fileprivate let mappableProperty: SubModel
+    fileprivate let arrayMappableProperty: [SubModel]
     
     fileprivate init(map: KeyedMapper<Model>) throws {
         try self.mappableProperty = map.from(.mappableProperty)
+        try self.arrayMappableProperty = map.from(.arrayMappableProperty)
     }
 }
 
@@ -53,6 +56,36 @@ class KeyedMapper_MappableSpec: QuickSpec {
                     let model: SubModel = try! mapper.from(.mappableProperty)
                     
                     expect(model).toNot(beNil())
+                }
+            }
+        }
+        
+        describe("from<T: Mappable> -> [T]") {
+            context("when the value in the JSON is not an array of NSDictionaries") {
+                it("should throw a typeMismatchError") {
+                    let expectedValue = 2
+                    let dict: NSDictionary = ["arrayMappableProperty" : expectedValue]
+                    let mapper = KeyedMapper<Model>(JSON: dict, type: Model.self)
+                    
+                    do {
+                        let _: [SubModel] = try mapper.from(.arrayMappableProperty)
+                    } catch let error as MapperError<Model> {
+                        expect(error) == MapperError.typeMismatchError(field: .arrayMappableProperty, forType: Model.self, value: expectedValue, expectedType: [NSDictionary].self)
+                    } catch {
+                        XCTFail("Error thrown from KeyedMapper<T: Mappable>.from was not a MapperError")
+                    }
+                }
+            }
+            
+            context("when the value in the JSON is an array of NSDictionaries") {
+                it("should map correctly") {
+                    let expectedValue = [["stringProperty" : ""]]
+                    let dict: NSDictionary = ["arrayMappableProperty" : expectedValue]
+                    let mapper = KeyedMapper<Model>(JSON: dict, type: Model.self)
+                    let models: [SubModel] = try! mapper.from(.arrayMappableProperty)
+                    
+                    expect(models).toNot(beNil())
+                    expect(models.count) == expectedValue.count
                 }
             }
         }
