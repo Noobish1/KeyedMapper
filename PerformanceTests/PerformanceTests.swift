bd919d348e3b8bd3a148d55566f0ee47b23f98f7
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-11-07 08:46:33 -0600

    added some performance tests for different safeValue implementations

--- PerformanceTests/PerformanceTests.swift
+++ PerformanceTests/PerformanceTests.swift
@@ -10,6 +10,8 @@ import XCTest
 import TestModels
 import KeyedMapper
 
+fileprivate let numberOfTests = 40000
+
 class KeyedMapper_Tests: XCTestCase {
     func testDeserialization() {
         self.measure {
@@ -32,4 +34,104 @@ class KeyedMapper_Tests: XCTestCase {
         let data = try! Data(contentsOf: path)
         return data
     }()
+
+    private lazy var keyedDict: NSDictionary = {
+        return ["user" : ["name" : "username"], "timestamp" : "123"]
+    }()
+
+    func testSafeValueWithExistingCode() {
+        let dict = keyedDict
+
+        self.measure {
+            for _ in 0...numberOfTests {
+                let wholeDict = self.existingSafeValue(forField: "", in: dict)
+                let username = self.existingSafeValue(forField: "user.name", in: dict)
+                let timestamp = self.existingSafeValue(forField: "timestamp", in: dict)
+
+                XCTAssertEqual(wholeDict as? NSDictionary, dict)
+                XCTAssertEqual(username as? String, "username")
+                XCTAssertEqual(timestamp as? String, "123")
+            }
+        }
+    }
+
+    func testSafeValueWithEmptyCheck() {
+        let dict = keyedDict
+
+        self.measure {
+            for _ in 0...numberOfTests {
+                let wholeDict = self.safeValueWithEmptyCheck(forField: "", in: dict)
+                let username = self.safeValueWithEmptyCheck(forField: "user.name", in: dict)
+                let timestamp = self.safeValueWithEmptyCheck(forField: "timestamp", in: dict)
+
+                XCTAssertEqual(wholeDict as? NSDictionary, dict)
+                XCTAssertEqual(username as? String, "username")
+                XCTAssertEqual(timestamp as? String, "123")
+            }
+        }
+    }
+
+    func testSafeValueWithEmptyCheckAndDotCheck() {
+        let dict = keyedDict
+
+        self.measure {
+            for _ in 0...numberOfTests {
+                let wholeDict = self.safeValueWithEmptyCheckAndDotCheck(forField: "", in: dict)
+                let username = self.safeValueWithEmptyCheckAndDotCheck(forField: "user.name", in: dict)
+                let timestamp = self.safeValueWithEmptyCheckAndDotCheck(forField: "timestamp", in: dict)
+
+                XCTAssertEqual(wholeDict as? NSDictionary, dict)
+                XCTAssertEqual(username as? String, "username")
+                XCTAssertEqual(timestamp as? String, "123")
+            }
+        }
+    }
+
+    private func existingSafeValue(forField field: String, in dictionary: NSDictionary) -> Any? {
+        var object: Any? = dictionary
+        var keys = field.characters.split(separator: ".").map(String.init)
+
+        while !keys.isEmpty, let currentObject = object {
+            let key = keys.removeFirst()
+            object = (currentObject as? NSDictionary)?[key]
+        }
+
+        return object
+    }
+
+    private func safeValueWithEmptyCheck(forField field: String, in dictionary: NSDictionary) -> Any? {
+        guard !field.isEmpty else {
+            return dictionary
+        }
+
+        var object: Any? = dictionary
+        var keys = field.characters.split(separator: ".").map(String.init)
+
+        while !keys.isEmpty, let currentObject = object {
+            let key = keys.removeFirst()
+            object = (currentObject as? NSDictionary)?[key]
+        }
+
+        return object
+    }
+
+    private func safeValueWithEmptyCheckAndDotCheck(forField field: String, in dictionary: NSDictionary) -> Any? {
+        guard !field.isEmpty else {
+            return dictionary
+        }
+
+        guard field.contains(".") else {
+            return dictionary[field]
+        }
+
+        var object: Any? = dictionary
+        var keys = field.characters.split(separator: ".").map(String.init)
+
+        while !keys.isEmpty, let currentObject = object {
+            let key = keys.removeFirst()
+            object = (currentObject as? NSDictionary)?[key]
+        }
+
+        return object
+    }
 }
