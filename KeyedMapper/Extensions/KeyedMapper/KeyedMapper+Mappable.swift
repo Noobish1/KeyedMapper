195743802e070c35209cb55946a1302193467be7
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-11-30 16:30:19 -0600

    added a from<T: Mappable>(_:) -> [[T]] function to KeyedMapper

--- KeyedMapper/Extensions/KeyedMapper/KeyedMapper+Mappable.swift
+++ KeyedMapper/Extensions/KeyedMapper/KeyedMapper+Mappable.swift
@@ -19,6 +19,15 @@ public extension KeyedMapper {
         }
 
         return try JSON.map { try T(map: KeyedMapper<T>(JSON: $0, type: T.self)) }
+
+    public func from<T: Mappable>(_ field: Object.Key) throws -> [[T]] {
+        let value = try JSON(fromField: field)
+
+        guard let JSON = value as? [[NSDictionary]] else {
+            throw MapperError.typeMismatch(field: field.stringValue, forType: Object.self, value: value, expectedType: [[NSDictionary]].self)
+        }
+
+        return try JSON.map { try [T].fromMap($0) }
     }
 
     public func optionalFrom<T: Mappable>(_ field: Object.Key) -> T? {
