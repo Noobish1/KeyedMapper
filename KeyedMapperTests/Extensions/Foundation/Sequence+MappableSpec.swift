58e270411e8c49bbc6ad785faab034f34368fa7f
Blair McArthur <blair.mcarthur@skipthedishes.com>
2016-12-01 15:14:52 -0600

    Added tests for the two sequence extensions.

---
+++ KeyedMapperTests/Extensions/Foundation/Sequence+MappableSpec.swift
@@ -0,0 +1,28 @@
+import Quick
+import Nimble
+@testable import KeyedMapper
+
+fileprivate struct Model: Mappable {
+    fileprivate enum Key: String, JSONKey {
+        case stringProperty
+    }
+
+    fileprivate let stringProperty: String
+
+    fileprivate init(map: KeyedMapper<Model>) throws {
+        try self.stringProperty = map.from(.stringProperty)
+    }
+}
+
+class Sequence_MappableSpec: QuickSpec {
+    override func spec() {
+        describe("Sequence<Mappable>.fromMap") {
+            it("should correctly map an array of mappable objects") {
+                let sequence: [NSDictionary] = [[Model.Key.stringProperty.rawValue : ""]]
+                let models = try! [Model].fromMap(sequence)
+
+                expect(models.count) == sequence.count
+            }
+        }
+    }
+}
