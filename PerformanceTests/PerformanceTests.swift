0f3d7c44a81d3351cecd2161286c8b9c91de5a7f
Blair McArthur <blair.mcarthur@skipthedishes.com>
2017-01-01 03:33:35 -0600

    updated the performance tests

--- PerformanceTests/PerformanceTests.swift
+++ PerformanceTests/PerformanceTests.swift
@@ -21,10 +21,10 @@ class KeyedMapper_Tests: XCTestCase {
     }
 
     func testPerformance() {
-        let dict = try! JSONSerialization.jsonObject(with: self.data, options: []) as! NSDictionary
+        let json = try! JSONSerialization.jsonObject(with: self.data, options: [])
 
         self.measure {
-            let programList = try! ProgramList.from(dictionary: dict)
+            let programList = try! ProgramList.from(json)
             XCTAssert(programList.programs.count > 1000)
         }
     }
@@ -34,104 +34,4 @@ class KeyedMapper_Tests: XCTestCase {
         let data = try! Data(contentsOf: path)
         return data
     }()
-
-    private lazy var keyedDict: NSDictionary = {
-        return ["user" : ["name" : "username"], "timestamp" : "123"]
-    }()
-
-    func testSafeValueWithExistingCode() {
-        let dict = keyedDict
-
-        self.measure {
-            for _ in 0...numberOfTests {
-                let wholeDict = self.existingSafeValue(forField: "", in: dict)
-                let username = self.existingSafeValue(forField: "user.name", in: dict)
-                let timestamp = self.existingSafeValue(forField: "timestamp", in: dict)
-
-                XCTAssertEqual(wholeDict as? NSDictionary, dict)
-                XCTAssertEqual(username as? String, "username")
-                XCTAssertEqual(timestamp as? String, "123")
-            }
-        }
-    }
-
-    func testSafeValueWithEmptyCheck() {
-        let dict = keyedDict
-
-        self.measure {
-            for _ in 0...numberOfTests {
-                let wholeDict = self.safeValueWithEmptyCheck(forField: "", in: dict)
-                let username = self.safeValueWithEmptyCheck(forField: "user.name", in: dict)
-                let timestamp = self.safeValueWithEmptyCheck(forField: "timestamp", in: dict)
-
-                XCTAssertEqual(wholeDict as? NSDictionary, dict)
-                XCTAssertEqual(username as? String, "username")
-                XCTAssertEqual(timestamp as? String, "123")
-            }
-        }
-    }
-
-    func testSafeValueWithEmptyCheckAndDotCheck() {
-        let dict = keyedDict
-
-        self.measure {
-            for _ in 0...numberOfTests {
-                let wholeDict = self.safeValueWithEmptyCheckAndDotCheck(forField: "", in: dict)
-                let username = self.safeValueWithEmptyCheckAndDotCheck(forField: "user.name", in: dict)
-                let timestamp = self.safeValueWithEmptyCheckAndDotCheck(forField: "timestamp", in: dict)
-
-                XCTAssertEqual(wholeDict as? NSDictionary, dict)
-                XCTAssertEqual(username as? String, "username")
-                XCTAssertEqual(timestamp as? String, "123")
-            }
-        }
-    }
-
-    private func existingSafeValue(forField field: String, in dictionary: NSDictionary) -> Any? {
-        var object: Any? = dictionary
-        var keys = field.characters.split(separator: ".").map(String.init)
-
-        while !keys.isEmpty, let currentObject = object {
-            let key = keys.removeFirst()
-            object = (currentObject as? NSDictionary)?[key]
-        }
-
-        return object
-    }
-
-    private func safeValueWithEmptyCheck(forField field: String, in dictionary: NSDictionary) -> Any? {
-        guard !field.isEmpty else {
-            return dictionary
-        }
-
-        var object: Any? = dictionary
-        var keys = field.characters.split(separator: ".").map(String.init)
-
-        while !keys.isEmpty, let currentObject = object {
-            let key = keys.removeFirst()
-            object = (currentObject as? NSDictionary)?[key]
-        }
-
-        return object
-    }
-
-    private func safeValueWithEmptyCheckAndDotCheck(forField field: String, in dictionary: NSDictionary) -> Any? {
-        guard !field.isEmpty else {
-            return dictionary
-        }
-
-        guard field.contains(".") else {
-            return dictionary[field]
-        }
-
-        var object: Any? = dictionary
-        var keys = field.characters.split(separator: ".").map(String.init)
-
-        while !keys.isEmpty, let currentObject = object {
-            let key = keys.removeFirst()
-            object = (currentObject as? NSDictionary)?[key]
-        }
-
-        return object
-    }
 }
